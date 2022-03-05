e7264b93f5ec36e25312cad8bd3553263338ff69
gghost2000 <43213159+gghost2000@users.noreply.github.com>
2022-03-06 02:13:04 +0900

    Time: 0 ms (100.00%), Space: 1.9 MB (90.73%) - LeetHub

--- 198-house-robber/198-house-robber.go
+++ 198-house-robber/198-house-robber.go
@@ -1,15 +1,19 @@
 func rob(nums []int) int {
-    n := len(nums)
-	dp := make([][2]int, 0)
-	dp = append(dp, [2]int{0, 0}, [2]int{nums[0], 0})
+	dp := make([]int, 0)
+	dp = append(dp, 0, nums[0])
 
-	i := 2
-	for i <= n {
-		dp = append(dp,[2]int{findMax(dp[i-2][0], dp[i-2][1]) + nums[i-1], findMax(dp[i-1][0], dp[i-1][1])})
-		i++
+
+	n := len(nums)
+
+	if n == 1 {
+		return nums[0]
 	}
-    
-    return findMax(dp[n][0], dp[n][1])
+
+	for i := 2; i <= n; i++ {
+		dp = append(dp, findMax(dp[i-1], dp[i-2]+nums[i-1]))
+	}
+
+	return findMax(dp[n], dp[n-1])
 
 }
 
