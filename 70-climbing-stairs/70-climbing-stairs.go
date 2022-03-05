60b7eebfd275700d0a231a4fb457d85d81f45855
gghost2000 <43213159+gghost2000@users.noreply.github.com>
2022-03-05 17:17:26 +0900

    Time: 2 ms (38.45%), Space: 1.9 MB (94.01%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.go
@@ -0,0 +1,15 @@
+func climbStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
+
+	var dp [46]int
+    dp[0] = 1
+	dp[1] = 1
+
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+
+	return dp[n]
+}
