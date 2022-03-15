759ccb0217dcb1b37ef62f22edc33ded6ca9a4fa
gghost2000 <43213159+gghost2000@users.noreply.github.com>
2022-03-15 22:10:09 +0900

    Time: 0 ms (100.00%), Space: 2.1 MB (49.88%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.go
@@ -0,0 +1,18 @@
+func getRow(rowIndex int) []int {
+    dp := make([][]int, 0)
+	dp = append(dp, []int{1})
+
+	for i := 1; i < rowIndex+1; i++ {
+		arr := make([]int, 0)
+		arr = append(arr, 1)
+
+		for j := 1; j < i; j++ {
+			arr = append(arr, dp[i-1][j-1]+dp[i-1][j])
+		}
+		arr = append(arr, 1)
+
+		dp = append(dp, arr)
+	}
+
+	return dp[rowIndex]
+}
