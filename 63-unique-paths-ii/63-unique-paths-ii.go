6481a74f8dfd0d2c64e809867e4463a31b42b0c8
gghost2000 <43213159+gghost2000@users.noreply.github.com>
2022-03-19 02:29:09 +0900

    Time: 0 ms (100.00%), Space: 2.6 MB (53.39%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.go
@@ -0,0 +1,42 @@
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	m, n := len(obstacleGrid), len(obstacleGrid[0])
+
+	dp := make([][]int, 0)
+
+	for i := 0; i < m; i++ {
+		arr := make([]int, n)
+
+		dp = append(dp, arr)
+	}
+
+
+	for i := 0; i < n; i++ {
+		if obstacleGrid[0][i] == 1 {
+			break
+		}
+
+		dp[0][i] = 1
+	}
+
+	for i := 0; i < m; i++ {
+		if obstacleGrid[i][0] == 1 {
+			break
+		}
+
+		dp[i][0] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+
+			if obstacleGrid[i][j] == 1 {
+				dp[i][j] = 0
+				continue
+			}
+
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+
+	return dp[m-1][n-1]
+}
